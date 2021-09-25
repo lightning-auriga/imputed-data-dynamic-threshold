cce36e678e3228b423a7ae25aabb75fc4138eced
Lightning Auriga <lightning.auriga@gmail.com>
2021-09-25 12:49:51 -0400

    feat: add main calls to binning methods

--- imputed-data-dynamic-threshold/main.cc
+++ imputed-data-dynamic-threshold/main.cc
@@ -38,6 +38,20 @@ int main(int argc, char **argv) {
   std::vector<std::string> info_files = ap.get_info_gz_files();
   double target_r2 = ap.get_target_average_r2();
   std::string output_filename = ap.get_output_filename();
+  imputed_data_dynamic_threshold::r2_bins bins;
+  std::cout << "creating MAF bins" << std::endl;
+  bins.set_bin_boundaries(maf_bin_boundaries);
+  std::cout << "iterating through specified info files" << std::endl;
+  for (std::vector<std::string>::const_iterator iter = info_files.begin();
+       iter != info_files.end(); ++iter) {
+    std::cout << "\t" << *iter << std::endl;
+    bins.load_info_file(*iter);
+  }
+  std::cout << "computing bin-specific r2 thresholds" << std::endl;
+  bins.compute_thresholds(target_r2);
+  std::cout << "reporting results to \"" << output_filename << "\""
+            << std::endl;
+  bins.report_thresholds(output_filename);
   std::cout << "all done woo!" << std::endl;
   return 0;
 }
