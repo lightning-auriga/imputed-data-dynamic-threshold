d565029a3445077e9e878665109297dc3c30ef7f
Lightning Auriga <lightning.auriga@gmail.com>
2023-11-19 10:55:03 -0500

    refactor: move primary program logic into separate class for eventual testing

---
+++ imputed-data-dynamic-threshold/executor.cc
@@ -0,0 +1,66 @@
+/*!
+  \file executor.cc
+  \brief implementation of wrapped program execution.
+  \copyright Released under the MIT License. Copyright
+  2023 Lightning Auriga
+ */
+
+#include "imputed-data-dynamic-threshold/executor.h"
+
+namespace iddt = imputed_data_dynamic_threshold;
+
+iddt::executor::executor() {}
+iddt::executor::~executor() throw() {}
+
+void iddt::executor::run(const std::vector<double> &maf_bin_boundaries,
+                         const std::vector<std::string> &info_files,
+                         const double &target_r2,
+                         const std::string &output_table_filename,
+                         const std::string &output_list_filename,
+                         bool second_pass,
+                         const std::string &filter_info_files_dir) {
+  imputed_data_dynamic_threshold::r2_bins bins;
+  std::cout << "creating MAF bins" << std::endl;
+  bins.set_bin_boundaries(maf_bin_boundaries);
+  std::cout << "iterating through specified info files" << std::endl;
+  for (std::vector<std::string>::const_iterator iter = info_files.begin();
+       iter != info_files.end(); ++iter) {
+    std::cout << "\t" << *iter << std::endl;
+    bins.load_info_file(*iter, !second_pass);
+  }
+  std::cout << "computing bin-specific r2 thresholds" << std::endl;
+  bins.compute_thresholds(target_r2);
+  std::ofstream output;
+  if (!output_table_filename.empty()) {
+    std::cout << "reporting tabular results to \"" << output_table_filename
+              << "\"" << std::endl;
+    output.open(output_table_filename.c_str());
+    if (!output.is_open())
+      throw std::runtime_error("cannot write to file \"" +
+                               output_table_filename + "\"");
+  } else {
+    std::cout << "reporting tabular results to terminal" << std::endl;
+  }
+  bins.report_thresholds(output_table_filename.empty() ? std::cout : output);
+  output.close();
+  output.clear();
+  if (!output_list_filename.empty()) {
+    output.open(output_list_filename.c_str());
+    if (!output.is_open())
+      throw std::runtime_error("cannot write to file \"" +
+                               output_list_filename + "\"");
+    std::cout << "reporting passing variants to \"" << output_list_filename
+              << "\"" << std::endl;
+    if (second_pass) {
+      for (std::vector<std::string>::const_iterator iter = info_files.begin();
+           iter != info_files.end(); ++iter) {
+        std::cout << "\t" << *iter << std::endl;
+        bins.report_passing_variants(*iter, filter_info_files_dir, output);
+      }
+    } else {
+      bins.report_passing_variants(output);
+    }
+    output.close();
+    output.clear();
+  }
+}
---
+++ imputed-data-dynamic-threshold/executor.h
@@ -0,0 +1,59 @@
+/*!
+  \file executor.h
+  \brief wrapped main execution of program for testing.
+  \copyright Released under the MIT License. Copyright
+  2021 Lightning Auriga
+ */
+
+#ifndef IMPUTED_DATA_DYNAMIC_THRESHOLD_EXECUTOR_H_
+#define IMPUTED_DATA_DYNAMIC_THRESHOLD_EXECUTOR_H_
+
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <random>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "imputed-data-dynamic-threshold/cargs.h"
+#include "imputed-data-dynamic-threshold/r2_bins.h"
+
+namespace imputed_data_dynamic_threshold {
+/*!
+ * \class executor
+ * \brief class handling primary program execution
+ */
+class executor {
+ public:
+  /*!
+   * \brief basic constructor
+   */
+  executor();
+  /*!
+   * \brief destructor
+   */
+  ~executor() throw();
+  /*!
+   * \brief run primary program logic
+   * \param maf_bin_boundaries maf values defining filtering regions
+   * \param info_files names of input files containing variant r2 data
+   * \param target_r2 target average r2 per bin after filtering
+   * \param output_table_filename name of file to which to report summary report
+   * \param output_list_filename name of file to which to report passing
+   * variants \param second_pass whether to save RAM and run in two passes
+   * \param filter_info_files_dir directory to which to write filtered files
+   */
+  void run(const std::vector<double> &maf_bin_boundaries,
+           const std::vector<std::string> &info_files, const double &target_r2,
+           const std::string &output_table_filename,
+           const std::string &output_list_filename, bool second_pass,
+           const std::string &filter_info_files_dir);
+};
+}  // namespace imputed_data_dynamic_threshold
+
+#endif  // IMPUTED_DATA_DYNAMIC_THRESHOLD_EXECUTOR_H_
